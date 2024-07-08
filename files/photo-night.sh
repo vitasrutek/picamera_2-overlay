12748d1c76005043fe6de01acbd5251f5c53e057
vitasrutek <98422550+vitasrutek@users.noreply.github.com>
2024-07-08 09:39:57 +0200

    Update photo-night.sh

--- files/photo-night.sh
+++ files/photo-night.sh
@@ -1,1 +1,9 @@
-TBC
+#!/bin/bash
+echo 'Mažu fotku'
+rm /home/pi/WEB/photo-night.jpg
+echo 'Vypínám službu'
+sudo systemctl stop camera.service
+echo 'Fotím'
+sudo -u pi python3 night.py
+echo 'Zapínám službu'
+sudo systemctl start camera.service
