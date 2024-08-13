a4b4e2690eb241a239f567c07a8eaf2ea2eb68b4
vitasrutek <98422550+vitasrutek@users.noreply.github.com>
2024-08-13 11:12:54 +0200

    Create web.py

---
+++ files_IMX708/web.py
@@ -0,0 +1,193 @@
+#!/usr/bin/python3
+
+import io
+import logging
+import socketserver
+import subprocess
+import os
+from http import server
+from threading import Condition, Timer
+import time
+import sys
+
+PORT = 80
+SHUTDOWN_INTERVAL = 30
+
+service_timer = None
+service_started = False
+
+os.chdir('/home/pi/WEB')
+
+def read_file(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def get_temperature():
+    result = subprocess.run(['vcgencmd', 'measure_temp'], stdout=subprocess.PIPE, text=True)
+    temp_output = result.stdout.strip()
+    temperature = temp_output.replace('temp=', '')
+    return temperature
+
+def get_venku():
+    result = subprocess.run(['/usr/bin/bash', '/home/pi/WEB/teplota.sh'], stdout=subprocess.PIPE, text=True)
+    return result.stdout.strip()
+
+def get_uptime():
+    result = subprocess.run(['uptime', '-p'], stdout=subprocess.PIPE, text=True)
+    return result.stdout.strip()
+
+html_template = read_file('/home/pi/WEB/index.html')
+content_file = '/home/pi/WEB/content.txt'
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def start_service(self):
+        global service_started, service_timer
+        if not service_started:
+            print("Spuštění služby pomocí systemctl...")
+            subprocess.run(["sudo", "systemctl", "start", "camera.service"])
+            service_started = True  # Označíme, že služba již byla spuštěna
+
+        if service_timer is not None:
+            service_timer.cancel()
+        service_timer = Timer(SHUTDOWN_INTERVAL, stop_service)
+        service_timer.start()
+
+    def do_GET(self):
+        self.start_service()
+        if self.path == '/':
+            try:
+                file_content = read_file(content_file)
+                uptime = get_uptime()
+                temperature = get_temperature()
+                venku = get_venku()
+                html_content = html_template.replace('{{ file_content }}', file_content)
+                html_content = html_content.replace('{{ uptime }}', uptime)
+                html_content = html_content.replace('{{ temperature }}', temperature)
+                html_content = html_content.replace('{{ venku }}', venku)
+                #self.start_service()
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(html_content.encode('utf-8'))
+            except Exception as e:
+                self.send_error(500, f"Internal server error: {e}")
+        if self.path == '/index.html':
+            try:
+                file_content = read_file(content_file)
+                uptime = get_uptime()
+                temperature = get_temperature()
+                venku = get_venku()
+                html_content = html_template.replace('{{ file_content }}', file_content)
+                html_content = html_content.replace('{{ uptime }}', uptime)
+                html_content = html_content.replace('{{ temperature }}', temperature)
+                html_content = html_content.replace('{{ venku }}', venku)
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(html_content.encode('utf-8'))
+            except Exception as e:
+                self.send_error(500, f"Internal server error: {e}")
+        elif self.path == '/photo-day.jpg':
+            try:
+                with open('/home/pi/WEB/photo-day.jpg', 'rb') as file:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'image/jpeg')
+                    self.end_headers()
+                    self.wfile.write(file.read())
+            except FileNotFoundError:
+                self.send_error(404, "File not found")
+        elif self.path == '/photo-hdr.jpg':
+            try:
+                with open('/home/pi/WEB/photo-hdr.jpg', 'rb') as file:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'image/jpeg')
+                    self.end_headers()
+                    self.wfile.write(file.read())
+            except FileNotFoundError:
+                self.send_error(404, "File not found")
+        elif self.path == '/photo-night.jpg':
+            try:
+                with open('/home/pi/WEB/photo-night.jpg', 'rb') as file:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'image/jpeg')
+                    self.end_headers()
+                    self.wfile.write(file.read())
+            except FileNotFoundError:
+                self.send_error(404, "File not found")
+        elif self.path == '/video.mp4':
+            try:
+                with open('/home/pi/WEB/video.mp4', 'rb') as file:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'video/mp4')
+                    self.end_headers()
+                    self.wfile.write(file.read())
+            except FileNotFoundError:
+                self.send_error(404, "File not found")
+
+    def do_POST(self):
+        if self.path == '/script_photo-day':
+            try:
+                result = subprocess.run(['/usr/bin/sudo', '/home/pi/WEB/photo-day.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                if result.returncode != 0:
+                    raise Exception(f"Error running script: {result.stderr}")
+                self.send_response(303)
+                self.send_header('Location', '/photo-day.jpg')
+                self.end_headers()
+            except Exception as e:
+                self.send_error(500, f"Internal server error: {e}")
+        if self.path == '/script_photo-hdr':
+            try:
+                result = subprocess.run(['/usr/bin/sudo', '/home/pi/WEB/photo-hdr.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                if result.returncode != 0:
+                    raise Exception(f"Error running script: {result.stderr}")
+                self.send_response(303)
+                self.send_header('Location', '/photo-hdr.jpg')
+                self.end_headers()
+            except Exception as e:
+                self.send_error(500, f"Internal server error: {e}")
+
+        elif self.path == '/script_photo-night':
+            try:
+                result = subprocess.run(['/usr/bin/sudo', '/home/pi/WEB/photo-night.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                if result.returncode != 0:
+                    raise Exception(f"Error running script: {result.stderr}")
+                self.send_response(303)
+                self.send_header('Location', '/photo-night.jpg')
+                self.end_headers()
+            except Exception as e:
+                self.send_error(500, f"Internal server error: {e}")
+
+        elif self.path == '/script_video':
+            try:
+                result = subprocess.run(['/usr/bin/sudo', '/home/pi/WEB/videoP.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                if result.returncode != 0:
+                    raise Exception(f"Error running script: {result.stderr}")
+                self.send_response(303)
+                self.send_header('Location', '/video.mp4')
+                self.end_headers()
+            except Exception as e:
+                self.send_error(500, f"Internal server error: {e}")
+
+def stop_service():
+    global service_started
+    if service_started:
+        print("Zastavení služby pomocí systemctl...")
+        subprocess.run(["sudo", "systemctl", "stop", "camera.service"])
+        service_started = False
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+try:
+    address = ('', PORT)
+    server = StreamingServer(address, StreamingHandler)
+
+    print("Server běží na portu ", PORT)
+    server.serve_forever()
+except KeyboardInterrupt:
+    print("\nZachyceno ukončení (Ctrl+C), vypínám...")
+finally:
+    if service_timer is not None:
+        service_timer.cancel()
+    stop_service()
