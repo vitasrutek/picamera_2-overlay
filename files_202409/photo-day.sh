4858e59749a442288f368ec1cb2fc91f30fb6b34
vitasrutek <98422550+vitasrutek@users.noreply.github.com>
2024-08-23 06:59:58 +0200

    Create photo-day.sh

---
+++ files_202409/photo-day.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo 'Mažu fotku'
+rm /home/pi/WEB/photo-day.jpg
+echo 'Vypínám službu'
+sudo systemctl stop camera.service
+echo 'Fotím'
+sudo -u pi rpicam-still --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx708_wide.json -t 10 --autofocus-on-capture 1  --autofocus-mode auto -q 100 -o /home/pi/WEB/photo-day.jpg #--rotation 180
+echo 'Zapínám službu'
+sudo systemctl start camera.service
