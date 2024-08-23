42741f1b861a526b50bbc8bc0cacdc201ec59f22
vitasrutek <98422550+vitasrutek@users.noreply.github.com>
2024-08-23 07:00:55 +0200

    Create photo-night.sh

---
+++ files_202409/photo-night.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo 'Mažu fotku'
+rm /home/pi/WEB/photo-night.jpg
+echo 'Vypínám službu'
+sudo systemctl stop camera.service
+echo 'Fotím'
+sudo -u pi rpicam-still -o /home/pi/WEB/photo-night.jpg --shutter 5000000 --gain 5 --awbgains 2,2 --immediate
+echo 'Zapínám službu'
+sudo systemctl start camera.service
